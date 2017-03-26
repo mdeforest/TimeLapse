be940e95f4552019bb8d2eb81cf8f4f7e1121f18
jsramsey <jramsey@fandm.edu>
2017-03-26 11:57:13 -0400

    Permssions for camera and writing external storage

--- app/src/main/java/com/example/michaela/timelapse/MainActivity.java
+++ app/src/main/java/com/example/michaela/timelapse/MainActivity.java
@@ -1,13 +1,104 @@
 package com.example.michaela.timelapse;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = MainActivity.class.getName();
+
+    private boolean canUseCamera;
+    private boolean canSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    public void recordPermission(){
+        //ask for permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        } else{
+            canUseCamera = true;
+        }
+    }
+
+    //code from https://developer.android.com/training/permissions/requesting.html
+    public void writeExternalPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        } else{
+            canSave = true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    canUseCamera =true;
+                } else {
+                    canUseCamera=false;
+                    // permission denied, boo! Disable the functionality that depends on this permission.
+                }
+            }
+            case 2: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (isExternalStorageWritable()) {
+                        File file = getAudioStorageDir("TimeLapse");
+                        canSave= true;
+                        loadSavedFiles();
+                    }
+                } else {
+                    canSave = false;
+                }
+            }
+        }
+    }
+
+    //loads all files in the directory to filessofar (so that they will all appear in ViewActivity)
+    public void loadSavedFiles(){
+        File sdCardRoot = Environment.getExternalStorageDirectory();
+        ArrayList filessofar = new ArrayList<>();
+        File yourDir = new File(sdCardRoot, "TimeLapse");
+        if (yourDir.listFiles() != null) {
+            for (File f : yourDir.listFiles()) {
+                if (f.isFile())
+                    filessofar.add(f.getName());
+            }
+        }
+    }
+
+    //gets the directory to store files in
+    public File getAudioStorageDir(String folderName){
+        File file = new File(Environment.getExternalStorageDirectory(), folderName);
+        if (!file.mkdirs()) {
+            Log.e(TAG, "Directory not created");
+        }
+        return file;
+    }
+
+    //determines if external storage is available
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
 }
