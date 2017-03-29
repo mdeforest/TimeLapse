51d2a6ae99a2fb0569dfd17aed5e6c7149985420
jsramsey <jramsey@fandm.edu>
2017-03-28 20:25:29 -0400

    CameraActivity- implementation of camera preview

--- app/src/main/java/com/example/michaela/timelapse/MainActivity.java
+++ app/src/main/java/com/example/michaela/timelapse/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.michaela.timelapse;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 
