fcbc0c84ea6284de6a835265e8dd1f81704b14a6
jsramsey <jramsey@fandm.edu>
2017-03-28 21:39:19 -0400

    camera cont

--- app/src/main/java/com/example/michaela/timelapse/CameraActivity.java
+++ app/src/main/java/com/example/michaela/timelapse/CameraActivity.java
@@ -3,16 +3,20 @@ package com.example.michaela.timelapse;
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Camera;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.FrameLayout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import static android.content.ContentValues.TAG;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static android.view.OrientationEventListener.ORIENTATION_UNKNOWN;
 import static com.example.michaela.timelapse.MainActivity.getCameraInstance;
 
 public class CameraActivity extends Activity {
@@ -35,6 +39,29 @@ public class CameraActivity extends Activity {
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
     }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions: ");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
 
     /**
      * A basic Camera preview class
--- app/src/main/java/com/example/michaela/timelapse/MainActivity.java
+++ app/src/main/java/com/example/michaela/timelapse/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.michaela.timelapse;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -12,6 +13,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Surface;
 import android.view.View;
 
 
@@ -37,11 +39,16 @@ public class MainActivity extends AppCompatActivity {
     /*
     * CAMERA FUNCTIONS
      */
-
     public void clickCapture(View v){
         Camera camera = null;
         if (checkCameraHardware(this)){
             camera = getCameraInstance(0);
+            Camera.Parameters parameters = camera.getParameters();
+            camera.setParameters(parameters);
+
+            //parameters.setRotation(180);
+            setCameraDisplayOrientation(this, 0, camera);
+            Log.d(TAG,"got here1");
 
         }
         Log.d(TAG, camera.toString());
@@ -52,6 +59,33 @@ public class MainActivity extends AppCompatActivity {
         camera.release();
 
     }
+
+
+    public static void setCameraDisplayOrientation(Activity activity, int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
+    }
+
+
+
+
 
            /*// Step 3: Set a CamcorderProfile (requires API Level 8 or higher)
         mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_TIME_LAPSE_HIGH));
